16332b6e0b8dc780065edf9913b8634129d0a287
ChoiMari <chl98639863@gmail.com>
2025-09-06 20:40:20 +0900

    feat : 프로그램 진입

--- MooMoney/src/main/java/io/github/choimari/moomoney/App.java
+++ MooMoney/src/main/java/io/github/choimari/moomoney/App.java
@@ -1,9 +1,17 @@
 package io.github.choimari.moomoney;
+
+import io.github.choimari.moomoney.factory.ViewFactory;
+import io.github.choimari.moomoney.util.InputReader;
+import io.github.choimari.moomoney.views.View;
+
 /**
  * 프로그램 진입점, Scanner 입력 처리, 메뉴 루프 담당 클래스
  */
 public class App { 
 	public static void main(String[] args) {
-		
+		InputReader reader = InputReader.getInstance();
+		ViewFactory factory = new ViewFactory(reader);
+		View main = factory.createView("main");
+		main.show();
 	}
 }
