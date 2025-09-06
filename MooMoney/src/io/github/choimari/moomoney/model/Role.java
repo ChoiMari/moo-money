1cf60303df6352755b3363ba08fd780bd9f51594
ChoiMari <chl98639863@gmail.com>
2025-09-06 11:46:47 +0900

    feat : 권한 추가
    일반회원, 프리미엄회원, 시스템 관리자 추가 #2

--- MooMoney/src/io/github/choimari/moomoney/model/Role.java
+++ MooMoney/src/io/github/choimari/moomoney/model/Role.java
@@ -2,12 +2,14 @@ package io.github.choimari.moomoney.model;
 
 public enum Role {
 	GUEST("비회원"), // public static final UserType = new UserType("비회원"); 과 같음. 공유 불변 객체 참조변수
-	MEMBER("회원"),
-	ADMIN("관리자");
+	REGULAR_MEMBER("일반 회원"), 
+    PREMIUM_MEMBER("프리미엄 회원"), 
+	ADMIN("관리자"),
+	SYSTEM("시스템 관리자");
 	
 	private final String label;
 	
-	Role(String label){  // 기본 private
+	private Role(String label){  
 		this.label = label;
 	}
 	
